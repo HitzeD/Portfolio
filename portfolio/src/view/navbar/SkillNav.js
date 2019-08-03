7efdc9060f50866a504277c961edbeaebecbd426
HitzeD <d-hitze@comcast.net>
2019-08-03 12:35:24 -0400

    added a navbar inside of skills

---
+++ portfolio/src/view/navbar/SkillNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SkillNav = () => {
+    return (
+        <div className="tabs">
+
+            <Link className="link" to='/home/skills'>
+                <h1>Skills Home</h1>
+            </Link>
+
+            <Link className="link" to='/home/skills/fe'>
+                <h1>Front-End</h1>
+            </Link>
+
+            <Link className="link" to='/home/skills/be'>
+                <h1>Back-End</h1>
+            </Link>
+
+        </div>
+    )
+}
+
+export default SkillNav;
