659b2a61d8b79bd0064e892e689b184b11f310d2
HitzeD <d-hitze@comcast.net>
2019-08-02 22:29:31 -0400

    added navbar into home page

---
+++ portfolio/src/view/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NavBar from '../navbar/NavBar';
+
+class Home extends React.Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div className="home-main">
+                <NavBar />
+            </div>
+        )
+    }
+}
+
+export default Home;
