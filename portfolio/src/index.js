525ad0e802f0781453dac14205e1c4dee014abe0
HitzeD <d-hitze@comcast.net>
2019-08-02 22:29:55 -0400

    added router

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppwithRouter = withRouter(App);
+ReactDOM.render(
+    <Router>
+        <AppwithRouter />
+    </Router>, 
+	document.getElementById('root')
+);
