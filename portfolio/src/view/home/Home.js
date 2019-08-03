161215f9bad35fc1237e501f5ffe868f8d7e2259
HitzeD <d-hitze@comcast.net>
2019-08-03 11:27:22 -0400

    all basic components added to route and verified

--- portfolio/src/view/home/Home.js
+++ portfolio/src/view/home/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import NavBar from '../navbar/NavBar';
+import SubNav from '../navbar/SubNav';
+import Welcome from '../welcome/Welcome';
+import Skills from '../skills/Skills';
+import Projects from '../projects/Projects';
+import Education from '../education/Education';
+import Contributions from '../contributions/Contributions';
+import me from '../../img/pro-pic.jpg';
 
 class Home extends React.Component {
     state = {
@@ -11,6 +19,24 @@ class Home extends React.Component {
         return(
             <div className="home-main">
                 <NavBar />
+                <div className="home-wrap">
+                    <div className="left">
+                        <div className="img-wrap">
+                            <img src={me} alt="Me" />
+                        </div>
+                        <div className="bottom-left">
+                            <SubNav />
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Route exact path='/home' component={Welcome} />
+                        <Route path='/home/skills' component={Skills} />
+                        <Route path='/home/projects' component={Projects} />
+                        <Route path='/home/education' component={Education} />
+                        <Route path='/home/contributions' component={Contributions} />
+                    </div>
+                </div>
+
             </div>
         )
     }
