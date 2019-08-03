d6f1f6b4d98cb9884fa3ced34981267b051dc7d4
HitzeD <d-hitze@comcast.net>
2019-08-03 14:19:47 -0400

    possible style

---
+++ portfolio/src/view/skillRoutes/intro/SkillIntro.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SkillIntro = () => {
+    return (
+        <div className="skillIntro-wrap">
+            <div className="skill-wrap">
+                <div className="title-wrap">
+                    <h1>Skills Home</h1>
+                </div>
+                <div className="para-wrap">
+                    <p>Above you find a nav that I created using React Router. 
+                      Navigate through those tabs to see what I have used and am currently using.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SkillIntro;
