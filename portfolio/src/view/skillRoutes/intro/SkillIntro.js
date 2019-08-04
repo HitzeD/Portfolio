b841ef57dc567ccdffe4fddcf032a4a0162fabd9
HitzeD <d-hitze@comcast.net>
2019-08-04 19:09:28 -0400

    updated skills home page

--- portfolio/src/view/skillRoutes/intro/SkillIntro.js
+++ portfolio/src/view/skillRoutes/intro/SkillIntro.js
@@ -3,15 +3,39 @@ import React from 'react';
 const SkillIntro = () => {
     return (
         <div className="skillIntro-wrap">
-            <div className="skill-wrap">
+            <div className="title-card">
                 <div className="title-wrap">
                     <h1>Skills Home</h1>
                 </div>
-                <div className="para-wrap">
-                    <p>Above you find a nav that I created using React Router. 
-                      Navigate through those tabs to see what I have used and am currently using.
-                    </p>
+            </div>
+            <div className="skills-wrap">
+                <div className="general">
+                    <div className="gen-wrap">
+                        <h3>General Skills</h3>
+                        <hr/>
+                        <ul className="gen-list">
+                            <li className="gen-item">Git</li>
+                            <li className="gen-item">CLI</li>
+                            <li className="gen-item">AJAX</li>
+                            <li className="gen-item">HTTP</li>
+                            <li className="gen-item">Netlify</li>
+                            <li className="gen-item">Heroku</li>
+                            <li className="gen-item">RESTful API</li>
+                        </ul>
+                    </div>            
                 </div>
+                <div className="practices">
+                    <div className="practice-wrap">
+                        <h3>Practices</h3>
+                        <hr/>
+                        <ul className="practice-list">
+                            <li className="practice-item">Object-Oriented Programming</li>
+                            <li className="practice-item">Agile</li>
+                            <li className="practice-item">Scrum</li>
+                        </ul>
+                    </div> 
+                </div>
+                
             </div>
         </div>
     )
