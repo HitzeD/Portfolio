fdd5c03afa113f96dceccc8bbe98cb25bdb477fb
HitzeD <d-hitze@comcast.net>
2019-08-04 19:50:22 -0400

    filled out Be page and styled

---
+++ portfolio/src/view/skillRoutes/backend/BackEnd.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const BackEnd = () => {
+    return (
+        <div className="be-main">
+            <div className="title-card">
+                <div className="title-wrap">
+                    <h1>Back-End Skills</h1>
+                </div>
+            </div>
+            <div className="be-wrap">
+                <div className="beSkill-wrap">
+                    <ul className="be-list">
+                        <li className="be-item">Java</li>
+                        <li className="be-item">Java Spring</li>
+                        <li className="be-item">Maven</li>
+                        <li className="be-item">Apache Tomcat</li>
+                        <li className="be-item">RabbitMQ</li>
+                        <li className="be-item">PostgreSQL</li>
+                        <li className="be-item">IntelliJ IDEA</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BackEnd;
