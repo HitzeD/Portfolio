2b1fbaaa21af2e671378dce5244326f6687fea8e
HitzeD <d-hitze@comcast.net>
2019-08-05 11:30:34 -0400

    set basic format and rough style

--- portfolio/src/view/contributions/Contributions.js
+++ portfolio/src/view/contributions/Contributions.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
+import ztm from '../../img/ztm.png';
+
 const Contributions = () => {
     return (
         <div className="con-main">
             <div className="title-wrap">
                 <h1>Contributions</h1>
             </div>
-            <div>
+            <div className="con-wrap">
+
+                <div className="con">
+                    <div className="con-left">
+                        <h2 className="title">Zero To Mastery WebPage</h2>
+                        <hr/>
+                        <div className="con-body">
+                            <p className="con-para">
+                                This was an Open Source contribution. In my early days of coding, 
+                                I started my learning career buying a course on <a href="https://www.udemy.com/the-complete-web-developer-zero-to-mastery/" target="_blank" rel="noopener noreferrer">Udemy</a> 
+                                by <a href="https://www.linkedin.com/in/andreineagoie/" target="_blank" rel="noopener noreferrer">Andrei Neagoie</a>. This website was by the students, for the students, 
+                                to show off and build with our skills.<br/><br/>
+                                
+                                On mobile, the container was hard coded rather than a responsive unit. 
+                                Thus it caused the content to spill out. I applied "vw" unit rather than "px".<br/><br/>
+                            
+                                I also recently went through, and scrubbed the page for all year dates, and 
+                                dynamically improved the page by removing human error and upkeep to the page.
+                                I created a function that locates all the years, and populates the field with 
+                                the current year.
+                            </p>
+                            <div className="con-btns">
+                                <a href="https://github.com/zero-to-mastery/zero-to-mastery-website">GitHub</a>
+                                <a href="https://zerotomastery.io/">Live Site</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="con-img">
+                        <img className="logo ztm" src={ztm} alt="Zero To Mastery Logo" />
+                    </div>
+                </div>
                 
             </div>
         </div>
--- portfolio/src/view/projects/ProjectList.js
+++ portfolio/src/view/projects/ProjectList.js
@@ -1,13 +1,72 @@
 import React from 'react';
 
+import mympy from '../../img/MIMPYlogo.svg';
+import immuno from '../../img/immunot.png';
+
 const ProjectList = () => {
     return (
         <div className="list-main">
             <div className="title-wrap">
                 <h1>Projects</h1>
             </div>
-            <div>
-                
+            <div className="pro-wrap">
+
+
+                <div className="project">
+                    <div className="pro-img">
+                        <img className="logo" src={mympy} alt="Mympy Logo" />
+                    </div>
+                    <div className="pro-right">
+                        <h2 className="title">MyMpy Dreams</h2>
+                        <hr/>
+                        <div className="pro-body">
+                            <p className="pro-para">
+                                Mympy Dreams is a marketplace where individuals can create 
+                                a profile and post their project and needs and find funding 
+                                from the Mymphy Dreams community.<br/><br/>
+
+                                Still Currently under development, and will be updated daily!
+                            </p>
+                            <div className="pro-btns">
+                                <a href="https://github.com/mympy-dreamers/Frontend">Front-End GitHub</a>
+                                <a href="https://github.com/mympy-dreamers/Backend">Back-End GitHub</a>
+                                <a href="https://mympydreamers.netlify.com/">Live Site</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="project">
+                    <div className="pro-img">
+                        <img className="logo" src={immuno} alt="ImmunoTracker Logo" />
+                    </div>
+                    <div className="pro-right">
+                        <h2 className="title">ImmunoTracker</h2>
+                        <hr/>
+                        <div className="pro-body">
+                            <p className="pro-para">
+                                This was the project I was assigned on my first build week at Lambda 	School. 
+                                My part of this was to create a marketing page that linked over via a button to 
+                                a React app created by the Front-End and Back-End Engineers. On the Home tab, I designed 
+                                a marketing page to 'sell our app' to the consumer. I used Javascript for the menu button. 
+                                I added an amination to the menu button which showcases animations.<br/><br/>
+
+                                On the About page, I was given the chance to write a statement, which was submitted by the 
+                                team. I added real Github links, and used the photos from Github as well. I enjoyed building 
+                                the accordion. It is a reusable accordion in the about page of my website that takes in an image, 
+                                title, name, and bio. With all components, the accordion successfully hides and shows the appropriate 
+                                content at the appropriate time.
+                            </p>
+                            <div className="pro-btns">
+                                <a href="https://github.com/immunizationtracking/userinterface">GitHub</a>
+                                <a href="https://immunot.netlify.com/">Live Site</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
