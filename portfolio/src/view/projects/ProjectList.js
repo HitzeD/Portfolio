0d21c27b3fddc2453e4dddb6223b38466901b409
HitzeD <d-hitze@comcast.net>
2019-08-04 21:12:19 -0400

    base component

---
+++ portfolio/src/view/projects/ProjectList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProjectList = () => {
+    return (
+        <div className="list-main">
+            <div className="title-wrap">
+                <h1>Projects</h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
